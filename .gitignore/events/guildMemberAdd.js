373829bb6bbf962bf95d067355c6f67828d3ad76
Horziox <43693672+Horziox@users.noreply.github.com>
2020-07-06 00:16:48 +0200

    Update guildMemberAdd.js

--- .gitignore/events/guildMemberAdd.js
+++ .gitignore/events/guildMemberAdd.js
@@ -7,6 +7,8 @@ module.exports = (bot, member) => {
     `Où est ${member.user} ? Dans le serveur !`,
     `**Swoooosh** !!! ${member.user} vient juste d'atterrir ! 🚀`,
     `${member.user} vient de rejoindre le serveur ! \\o/`,
+    `Bah tiens mais qui voilage ? Petit(e) ${member.user}... :musical_note:`,
+    `La légende disait donc vraie !!?\n${member.user} viens juste de rejoindre le serveur ! :scream:`
   ];
   member.roles.add("729242344733278249")
   let bvn = msg[Math.floor(Math.random() * msg.length)];
