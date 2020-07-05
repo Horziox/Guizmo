0f674cb5a06620985af862eb6cbb1ca576020356
Horziox <43693672+Horziox@users.noreply.github.com>
2020-07-06 00:10:16 +0200

    Update guildMemberAdd.js

--- .gitignore/events/guildMemberAdd.js
+++ .gitignore/events/guildMemberAdd.js
@@ -5,12 +5,13 @@ module.exports = (bot, member) => {
     `Hey salut !:partying_face:\nJ'espère que tu as pensé(e) à la pizza ${member.user} ?:sweat_smile:`,
     `Hé ! Écoutez ! ${member.user} nous a rejoint !`,
     `Où est ${member.user} ? Dans le serveur !`,
-    `**Swoooosh** !!! ${member.user} vient juste d'atterrir.`,
+    `**Swoooosh** !!! ${member.user} vient juste d'atterrir ! 🚀`,
     `${member.user} vient de rejoindre le serveur ! \\o/`,
   ];
   member.roles.add("729242344733278249")
   let bvn = msg[Math.floor(Math.random() * msg.length)];
   let embed = new Discord.MessageEmbed()
+  .setTitle(member.user.username)
   .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
   .setDescription(bvn)
   .setColor("7ED321")
