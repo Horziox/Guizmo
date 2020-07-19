e1525edda130fabf9dca4370cd87ecc44c5c6670
Horziox <43693672+Horziox@users.noreply.github.com>
2020-07-19 14:36:52 +0200

    Add stats command

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
         .setColor('#bf9322')
         .addField("Générales", `**${prefix}help** Affiche ce message ! :tada:\n**${prefix}info** Affiche des informations sur le serveur, toi, un utilisateur et surtout MOI !\n**${prefix}pfc** Pierre, feuille ciseau ? :eyes:`)
         .addField("Roles", `Donnez vous ou retirez vous manuellement des rôles : \n**${prefix}apex**\n**${prefix}fortnite**\n**${prefix}minecraft**`)
-        .addField("Fortnite", `**${prefix}news** Donne les actualités en jeux ! <:fortnite:551690476390973466>`)
+        .addField("Fortnite", `**${prefix}news** Donne les actualités en jeux ! <:fortnite:551690476390973466>\n**${prefix}stats** Affiche le menu d'aide pour afficher vos stats Fortnite`)
         .setFooter(`${bot.user.username} bêta`, bot.user.displayAvatarURL())
         .setTimestamp()
         message.channel.send(embed)
