0aaf78840a6c66eeff9406ce2ffbe4364a3d91f4
Horziox <horziox.dev@gmail.com>
2020-07-20 08:45:57 +0200

    Delete useless commentary

--- .gitignore/commands/pfc.js
+++ .gitignore/commands/pfc.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports = {
     name: "pfc",
     execute(message, args, bot, prefix) {
-        //if((args[0].toLowerCase() == "pierre") || (args[0].toLowerCase() == "feuille") || (args[0].toLowerCase() == "ciseaux")) return message.reply("essaie pas de tricher !\nJe sais jouer à pfc, c'est sois Pierre, soit Feuille ou soit Ciseaux et rien d'autre ! <a:shakeeyes:729436288599720028>")
         let pfc = ["pierre","feuille","ciseaux"]
         let random = pfc[Math.floor(Math.random() * pfc.length)];
         let embed = new Discord.MessageEmbed()
@@ -16,10 +15,10 @@ module.exports = {
         let resE = ["Ouais égalité, t'as de la chance, c'est tout, rien de plus...","**Arrêtes de me copier !!!** :face_with_symbols_over_mouth:","Mouais... Je t'ai à l'oeil ! :eyes:"]
         let randomEqu = resE[Math.floor(Math.random() * resE.length)];
 
-        let resV = ["Ahahaha !!! T'es trop null(e) ! :rofl:","Tu veux que je te dise ?\nPoint faible ? Trop fort écoutes, c'est comme ça, j'y peux rien ! ^^","Je savais que t'allais jouer "]//+args[0].chartAt(0).toUpperCase()+args[0].substring(1).toLowerCase()+" t'es trop prévisible quoi, change ton jeu..."]
+        let resV = ["Ahahaha !!! T'es trop null(e) ! :rofl:","Tu veux que je te dise ?\nPoint faible ? Trop fort écoutes, c'est comme ça, j'y peux rien ! ^^","Je savais que t'allais jouer "]
         let randomVic = resV[Math.floor(Math.random() * resV.length)];
 
-        let resD = ["Mais arrêtes de tricher !!!","T'as juste eu de la chance, calme toi... :triumph:","Je savais que t'allais jouer "]//+args[0].chartAt(0).toUpperCase()+args[0].substring(1).toLowerCase()+" t'es trop prévisible quoi, je te laisses gagner..."]
+        let resD = ["Mais arrêtes de tricher !!!","T'as juste eu de la chance, calme toi... :triumph:","Je savais que t'allais jouer "]
         let randomDef = resD[Math.floor(Math.random() * resD.length)];
 
         if(args[0].toLowerCase() === random) {
